f8d6d2cef5d77ec97062be697f2f83ccb4132a99
Albert Remalin Raja <albertpaulus836@gmail.com>
2023-04-11 18:41:40 +0530

    user route modified

--- controller/dummy.js
+++ controller/dummy.js
@@ -1,20 +1,5 @@
-home:async (req, res,next) => { 
-    // // app.get('/:id', function (req, res) {
-    //     console.log(req.params['id']);
-    //     res.send();
-    //   // });
-    let user = req?.session?.user;
-    console.log("mone", user);
-    let cartCount= await cartHelpers?.getCartCount(user)
-    //CHANGED THE STRUCTURE OF THIS CODE NORMAL RENDER VS PRODUCTS COMING
-    productHelpers.getAllProducts().then((product)=>{
-      console.log(cartCount,"cartCount")
-      res.render("user/user", { 
-        user,
-        cartCount
-       });
-    })
-    .catch((err)=>{
-      console.log(err,"ERROR")
-    })
-  }
+hi(4,5,6);
+
+function hi(hi,hello,ki){
+    console.log(hi,hello,ki)
+}
--- routes/user.js
+++ routes/user.js
@@ -1,78 +1,67 @@
 const express = require("express");
 const router = express.Router();
-const userController=require("../controller/userController")
+const userController = require("../controller/userController");
 // const bodyParser = require("body-parser");
-const db=require("../model/connection")
-const auth=require("../controller/auth")
+const db = require("../model/connection");
+const auth = require("../controller/auth");
 
-
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 const adminController = require("../controller/adminController");
-
-// router.post('/login',
-//     body('email').isEmail().normalizeEmail(),
-//     body('password').isLength({
-//         min: 6
-//     }),
-//     (req, res) => {
-//         const errors = validationResult(req);
-
-//         if (!errors.isEmpty()) {
-//             return res.status(400).json({
-//                 success: false,
-//                 errors: errors.array()
-//             });
-//         }
-
-//         res.render("admin/admin-dashboard")
-//         res.status(200).json({
-//             success: true,
-//             message: 'Login successful',
-
-//         })
-//     });
-
-
-
-
+const verifyLogin = require("../controller/auth");
 /* GET home page. */
-router.get("/",userController.home)
+router.get("/", userController.home);
 // GET sign up//
 
-router.get("/signup",userController.getUserSignup);
+router.get("/signup", userController.getUserSignup);
 
 // POST sign up//
 
-router.post("/signup",userController.postUsersignup);
+router.post("/signup", userController.postUsersignup);
 
 //GET LOGIN//
 
-router.get("/login",userController.getUsersLogin)
+router.get("/login", userController.getUsersLogin);
 
 //POST LOGIN//
 
-router.post("/login",userController.postUserSignin)
-//LOGOUT//  
-router.get("/logout",userController.getUserlogout)
+router.post("/login", userController.postUserSignin);
+//LOGOUT//
+router.get("/logout", userController.getUserlogout);
 //SHOP//
-router.get("/shop",userController.shopPage)
+router.get("/shop", verifyLogin.verifyLogin, userController.shopPage);
 //VIEW-PRODUCT//
-router.get('/view-product',userController.getProductPage)
+router.get("/view-product", userController.getProductPage);
 //CART//
-router.get("/cart",userController.Cart)
+router.get("/cart", verifyLogin.verifyLogin, userController.Cart);
 //EMPTY CART
-router.get("/emptyCart",userController.EmptyCart)
+router.get("/emptyCart", verifyLogin.verifyLogin, userController.EmptyCart);
 //add To Cart//
-router.get("/add-to-cart/:id",userController.getAddToCart)
+router.get("/add-to-cart/:id", userController.getAddToCart);
 //delete  Cart//
 // router.post("/delete_cart/:id",userController.deleteCartProduct)
 
 //change Product quantity
-router.post("/change-product-quantity",userController.changeProductQuantity)
+router.post("/change-product-quantity", userController.changeProductQuantity);
 //remove from Cart
-router.delete('/remove-cart',userController.removeCart)
+router.delete("/remove-cart", userController.removeCart);
 //order page
-router.get('/order',userController.Order)
-
-
+router.get("/order", verifyLogin.verifyLogin, userController.getOrder);
+//Place Order
+router.post("/order", userController.placeOrder);
+//Order Success:
+router.get(
+  "/order-success",
+  verifyLogin.verifyLogin,
+  userController.orderSucesss
+);
+//Order Success: post
+router.post("/cancel-order", userController.cancelOrder);
+//filter product
+router.get("/filter/:id", userController.filterProduct);
+//address
+router.delete("/delete-address/:id", userController.deleteAddress);
+//profile page
+router.get("/profile", userController.getprofilePage);
+///verify_payment
+router.post("/verify_payment",userController.verifypayment)
 module.exports = router;
