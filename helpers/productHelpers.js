cd65c0a91f21a714e7159af500519e4f5f5b9a50
Albert Remalin Raja <albertpaulus836@gmail.com>
2023-04-13 14:45:14 +0530

    proHelper modified

--- helpers/productHelpers.js
+++ helpers/productHelpers.js
@@ -1,38 +1,35 @@
 const { response } = require("express");
 // const { tryParse } = require("objectid");
-const db=require("../model/connection")
+const db = require("../model/connection");
 //IMAGE UPLOAD
 const { products, category } = require("../model/connection");
 
-var fs = require('fs');
-var path = require('path');
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
- 
-
+var fs = require("fs");
+var path = require("path");
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 module.exports = {
   // Get All Product
 
-   getAllProducts: () => {
+  getAllProducts: () => {
     return new Promise(async (resolve, reject) => {
-  try{ 
-    let products=await db.products.find({});
-    console.log(products)
-      resolve(products)
-      // console.log(products)
-    }catch(error){
-      console.log(error)
-    }});
+      try {
+        let products = await db.products.find({});
+        // console.log(products)
+        resolve(products);
+      } catch (error) {
+        console.log(error);
+      }
+    });
   },
 
   getProduct: (proId) => {
-
     return new Promise((resolve, reject) => {
       try {
-        db.products.find({_id:proId}).then((products) => {
-          console.log(products)
+        db.products.find({ _id: proId }).then((products) => {
+          console.log(products);
           resolve(products);
         });
       } catch (error) {
@@ -40,91 +37,127 @@ module.exports = {
       }
     });
   },
-  
-   // Add Product
 
-   addProduct: (products) => {
-    
-    return new Promise(async(resolve, reject) => {
+  // Add Product
+
+  addProduct: (products) => {
+    return new Promise(async (resolve, reject) => {
       try {
-        
-        
+        console.log("asjkajskajkajskajsklajsklajska",products)
         let data = await db.products(products);
-      
+        
         data.save();
         resolve(data._id);
-        console.log("data",data._id);
-        
-
+        console.log("dataaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", data._id);
       } catch (error) {
         console.log(error);
       }
     });
   },
-  
-  postAddProduct: (userData,filename)=>{
-    return new Promise((resolve,reject)=>{
-      
-        uploadedImage= new db.products({
-            name:userData.name,
-            description:userData.description,
-            price:userData.price,
-            image:filename,
-            
-        })
-        uploadedImage.save().then((data)=>{
-            resolve(data)
-        })
-    })
-},
 
-    // Update Product
+  postAddProduct: (product) => {
+    return new Promise((resolve, reject) => {
+      console.log(filename);
+      db.products.create(
+        {
+          name: userData.name,
+          description: userData.description,
+          price: userData.price,
+        },
+        (err, data) => {
+          if (err) {
+            console.log("errorrrrrrr", err);
+          } else {
+            resolve(data);
+          }
+        }
+      );
+    });
+  },
+
+  // Update Product
 
   updateProduct: (proId, body) => {
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
       try {
-        console.log("THISSSSSSSSSSSSS",body)
-        db.products.updateOne(
-          {_id:proId},
-          {
-            $set: {
-              name: body.name,
-              description: body.description,
-              price: body.price,
-              category: body.category,
-              
-              
-            },
-          }
-        ).then(()=>{
-          resolve();
-        })
-        
+        console.log("THISSSSSSSSSSSSS", body);
+        db.products
+          .updateOne(
+            { _id: proId },
+            {
+              $set: {
+                name: body.name,
+                description: body.description,
+                price: body.price,
+                category: body.category,
+              },
+            }
+          )
+          .then(() => {
+            resolve();
+          });
       } catch (error) {
         console.log(error);
       }
     });
   },
-  
- 
-  deleteProduct:(proId)=>{
-    console.log("in helper:",proId)
-    return new Promise (async(resolve,reject)=>{
-      try{
-        await db.products.deleteOne({_id:proId});
+
+  deleteProduct: (proId) => {
+    console.log("in helper:", proId);
+    return new Promise(async (resolve, reject) => {
+      try {
+        await db.products.deleteOne({ _id: proId });
         // console.log(_id)
         resolve();
+      } catch (error) {
+        console.log(error);
       }
-      catch(error){
-        console.log(error)      }
-    })
+    });
+  },
+
+  unlistProduct: (proId) => {
+    return new Promise((resolve, reject) => {
+      try {
+        db.products
+          .updateOne(
+            { _id: proId },
+            {
+              $set: {
+                status: false,
+              },
+            }
+          )
+          .then(() => {
+            resolve();
+          });
+      } catch (error) {
+        console.log("ERROR IN UNlist PRODUCT", error);
+      }
+    });
+  },
+  listProduct: (proId) => {
+    try {
+      return new Promise((resolve, reject) => {
+        db.products.updateOne(
+          { _id: proId },
+          {
+            $set: {
+              status: true,
+            },
+          }
+        );
+      });
+    } catch (error) {
+      console.log("ERROR IN LIST PRODUCT", error);
+    }
   },
   //CATEGORY
 
-   // Add Category
+  // Add Category
 
-   addCategory: (categories) => {
+  addCategory: (categories) => {
     return new Promise((resolve, reject) => {
+      console.log("1234567",categories)
       db.category
         .find({
           category: categories.category,
@@ -133,6 +166,7 @@ module.exports = {
           console.log(response);
           if (response.length == 0) {
             try {
+              
               await db.category(categories).save();
               resolve();
             } catch (error) {
@@ -145,7 +179,7 @@ module.exports = {
         });
     });
   },
-   // Get All Category
+  // Get All Category
 
   getAllcategory: () => {
     return new Promise(async (resolve, reject) => {
@@ -158,25 +192,25 @@ module.exports = {
     });
   },
   deleteCategory: (cateId) => {
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       try {
-        console.log("hiiiii")
-         await db.category.deleteOne({_id:cateId});
+        console.log(cateId);
+        await db.category.deleteOne({ _id: cateId });
         resolve();
       } catch (error) {
-        console.log("hiiiii")
+        console.log("hiiiii");
 
         console.log(error);
       }
     });
   },
-  
+
   //NO ASYNC AWAIT
   getCategory: (cateId) => {
     return new Promise(async (resolve, reject) => {
       try {
-        await db.category.find({_id:cateId}).then((category)=>{
-        resolve(category);
+        await db.category.find({ _id: cateId }).then((category) => {
+          resolve(category);
         });
       } catch (error) {
         console.log(error);
@@ -184,55 +218,61 @@ module.exports = {
       }
     });
   },
-  updateCategory:(cate,cateId)=>{
-    return new Promise(async(resolve,reject)=>{
-     try{
-     await db.category.updateOne(
-        {_id:cateId},
-        {$set:{
-          category:cate.category
-        }}
-      )
-      resolve();
-       
-       
-     
-     }
-     catch{
-
-     }
-    })
+  updateCategory: (cate, cateId) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await db.category.updateOne(
+          { _id: cateId },
+          {
+            $set: {
+              category: cate.category,
+            },
+          }
+        );
+        resolve();
+      } catch {}
+    });
   },
 
   //USERS
-  getAllUsers:()=>{
-    return new Promise (async(resolve,reject)=>{
-      try{
-       let user= await db.user.find({});
-        resolve(user)
-      }catch(error){
-        console.log(error);
-      }
-    })
-  },
-  blockUsers:async(userId)=>{
-    const response=await new Promise((resolve,reject)=>{
+  getAllUsers: () => {
+    return new Promise(async (resolve, reject) => {
       try {
-        let data =  db.user(user);
-      
-         db.user.updateOne({_id:userId},
-          {
-            $set:{
-            isBlocked:true
-            }
-          })
-
+        let user = await db.user.find({});
+        resolve(user);
       } catch (error) {
         console.log(error);
       }
+    });
+  },
+  blockUsers:  (userId) => {
+    const response =  new Promise(async(resolve, reject) => {
+      try {
+        let data = db.user(user);
 
+        db.user.updateOne(
+          { _id: userId },
+          {
+            $set: {
+              isBlocked: true,
+            },
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    resolve(response);
+  },
+  filterCategory: (catName) => {
+   return new Promise (async(resolve,reject)=>{
+    
+     let cat=await db.products.find({category:catName})
+    //  console.log(cat,"jskajalskjalksjsa")
+      resolve(cat)
+      
     })
-    resolve(response)
-  }
-}
+    
+  },
 
+};
