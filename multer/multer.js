2d05027069be634dc2cdc8268b1f398499c5c9ce
Albert Remalin Raja <albertpaulus836@gmail.com>
2023-04-26 20:40:13 +0530

    Create multer.js

---
+++ multer/multer.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+
+const Storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const editedStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+module.exports = {
+  uploads: multer({ storage: Storage }).array("file", 4),
+  editeduploads: multer({ storage: editedStorage }).fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+};
