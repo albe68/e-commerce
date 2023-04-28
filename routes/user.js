812a42763e12f009c919563358bbde36986dfbde
Albert Remalin Raja <albertpaulus836@gmail.com>
2023-04-28 11:25:08 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,67 +1,71 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controller/userController");
 // const bodyParser = require("body-parser");
 const db = require("../model/connection");
 const auth = require("../controller/auth");
-
 const { body, validationResult } = require("express-validator");
+const userController = require("../controller/userController");
 const adminController = require("../controller/adminController");
 const verifyLogin = require("../controller/auth");
-/* GET home page. */
-router.get("/", userController.home);
-// GET sign up//
+const productController = require("../controller/productController");
+const cartController = require("../controller/cartController");
+const orderController = require("../controller/orderController");
+const wishlistController = require("../controller/wishlistController");
 
+//SIGNUP
 router.get("/signup", userController.getUserSignup);
-
-// POST sign up//
-
 router.post("/signup", userController.postUsersignup);
 
-//GET LOGIN//
-
+//user login
 router.get("/login", userController.getUsersLogin);
-
-//POST LOGIN//
-
 router.post("/login", userController.postUserSignin);
-//LOGOUT//
 router.get("/logout", userController.getUserlogout);
-//SHOP//
-router.get("/shop", verifyLogin.verifyLogin, userController.shopPage);
-//VIEW-PRODUCT//
-router.get("/view-product", userController.getProductPage);
-//CART//
-router.get("/cart", verifyLogin.verifyLogin, userController.Cart);
-//EMPTY CART
-router.get("/emptyCart", verifyLogin.verifyLogin, userController.EmptyCart);
-//add To Cart//
-router.get("/add-to-cart/:id", userController.getAddToCart);
-//delete  Cart//
-// router.post("/delete_cart/:id",userController.deleteCartProduct)
 
-//change Product quantity
-router.post("/change-product-quantity", userController.changeProductQuantity);
-//remove from Cart
-router.delete("/remove-cart", userController.removeCart);
-//order page
-router.get("/order", verifyLogin.verifyLogin, userController.getOrder);
-//Place Order
-router.post("/order", userController.placeOrder);
-//Order Success:
+//otp services
+router.get("/otp-login", userController.getUserOtpLogin);
+router.post("/otp-login", userController.postUserOtpLogin);
+router.get("/otp_verify", userController.getOtpVerify);
+router.post("/otp_verify", userController.postOtpVerify);
+
+//VIEW-PRODUCT//
+router.get("/", userController.home);
+router.get("/shop", verifyLogin.verifyLogin, userController.shopPage);
+router.get("/view-product/:id", productController.getProductPage);
+router.get("/filter/:id", productController.filterProduct);
+router.post("/search", productController.getSearch);
+router.post("/sort", productController.sortProducts);
+//dark mode
+router.patch("/dark-mode", userController.darkMode);
+
+//CART//
+router.get("/cart", verifyLogin.verifyLogin, cartController.Cart);
+router.get("/check-cart-quantity/:id", cartController.checkCartQuantity);
+router.get("/emptyCart", verifyLogin.verifyLogin, cartController.EmptyCart);
+router.get("/add-to-cart/:id", cartController.getAddToCart);
+router.post("/change-product-quantity", cartController.changeProductQuantity);
+router.delete("/remove-cart", cartController.removeCart);
+
+//ORDER
+router.get("/order", verifyLogin.verifyLogin, orderController.getOrder);
+router.post("/order", orderController.placeOrder);
 router.get(
   "/order-success",
   verifyLogin.verifyLogin,
-  userController.orderSucesss
+  orderController.orderSucesss
 );
-//Order Success: post
-router.post("/cancel-order", userController.cancelOrder);
-//filter product
-router.get("/filter/:id", userController.filterProduct);
-//address
-router.delete("/delete-address/:id", userController.deleteAddress);
+router.post("/cancel-order", orderController.cancelOrder);
+router.post("/verify_payment", orderController.verifypayment);
+// router.post("/verify_coupon", orderController.verifyCoupon);
+
+//  wISHLIST
+
+router.get("/add_to_wishlist/:id", wishlistController.addToWishlist);
+router.get("/wishlist", wishlistController.getWishlist);
+router.delete("/wishlist/remove/:id", wishlistController.deleteWishlist);
+
 //profile page
 router.get("/profile", userController.getprofilePage);
-///verify_payment
-router.post("/verify_payment",userController.verifypayment)
+router.post("/change-passsword", userController.changePassword);
+router.delete("/delete-address/:id", userController.deleteAddress);
+
 module.exports = router;
